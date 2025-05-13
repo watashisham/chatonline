bbaa9c7bdaed749165f465621e0eb23fe1e7a16c
Sham <wata5hi@proton.me>
2025-05-13 19:56:08 -0300

    servidor configurado

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,3 +1,21 @@
 const { WebSocketServer} = require("ws")
+const dotenv = require("dotenv")
 
-const wss = new WebSocketServer({ port : 8080})
+dotenv.config()
+
+const wss = new WebSocketServer({ port: process.env.PORT || 8080})
+
+//SEMPRE QUE ALGUÉM CONECTAR EXECUTAR
+wss.on("connection", (ws) => {
+
+//SEMPRE QUE DER ERRO EXECUTAR
+    ws.on("error", console.error)
+
+    //SEMPRE QUE ALGUÉM ENVIAR UMA MENSAGEM EXECUTAR
+    ws.on("message", () => {
+        //ENVIAR MENSAGEM PARA TODOS OS CLIENTES
+        wss.clients.forEach((clients) => clients.send(data.toString()))      
+    })
+
+    console.log("Client conected.")
+})
